17107203206625cedc822286f66d68eb792d8f31
Kanwaljit Singh <s_kanwaljit@hotmail.com>
2014-09-12 23:03:16 -0700

    Game word order - check answer, hint and next working, link from index

--- game_word_order.php
+++ game_word_order.php
@@ -34,13 +34,64 @@ header('Content-Type: text/html; charset=utf-8');
 
 	function hint()
 	{
-		alert("Seriously, you need Hint? It's only from first 13 pages of Guru Granth Sahib Ji (NitNem)");
+		
+		//alert("Seriously, you need Hint? It's only from first 13 pages of Guru Granth Sahib Ji (NitNem)");
+
+		
+		
+		var ans = check(false);
+		if(ans==-1)
+			alert('Check your answer, you should feel good.');
+		else
+			alert('You need to check the word at position ' + (ans+1) );
+		
 	}
 
-	function check()
+	function check(warn)
 	{
-		alert("Working on this feature.");
+		//console.log(a);
+		
+		var result=true;
+		var b=new Array();
+		
+		$('#sortable li').each(function() {
+			b.push($( this ).html());
+		});
+
+		//console.log(b);
+		
+		var word_index=0;
+		for(var i=0; i < b.length; i++)
+		{
+			c=b[i];
+			//console.log(c);
+			if(a[word_index]!=c)
+			{
+				if(warn)
+					alert('Wrong, try again !');
+				
+				result=false;
+				return word_index;
+			} else {
+				result=true;
+				word_index++;
+			}
+		}
+		
+		if(result)
+		{
+			if(warn)
+				alert("Great, you are correct !");
+			
+			return -1;
+		}
+
 	}	
+
+	function next()
+	{
+		location.reload();
+	}
   
   </script>
 </head>
@@ -62,11 +113,20 @@ if (!$con->set_charset("utf8")) {
     //printf("Current character set: %s\n", $con->character_set_name());
 }
 
-$result = mysqli_query($con,"SELECT text, id, page, line FROM scriptures where page<=13 order by RAND() LIMIT 1");
+$pages=13;
+if($_GET['level']=="5")
+{
+	$pages = 1430;
+}
+
+$result = mysqli_query($con,"SELECT text, id, page, line FROM scriptures where page<=$pages order by RAND() LIMIT 1");
 
 $row = mysqli_fetch_array($result);
 $line = $row['text'];
 $words = explode(' ',$line);
+
+$actual=$words;
+
 shuffle($words);
 echo "<!-- ".$line." -->";
 //echo "<br>";
@@ -89,10 +149,19 @@ foreach ($words as $word)
 
 <hr/>
 
-<button onclick="check()">Check Answer</button>
+<button onclick="check(true)">Check Answer</button>
 <br/><br/>
 <button onclick="hint()">Hint</button>
+<br/><br/>
+<button onclick="next()">Next</button>
  
  
+ 
+
 </body>
+
+<script>
+	var a=['<?php echo implode("','",$actual); ?>'];
+	</script>
+
 </html>
