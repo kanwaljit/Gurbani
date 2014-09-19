f09660045b1ac1ef23c07c1c6e03977ce5ef7f8b
Kanwaljit Singh <kanwaljit@silvertipsoftware.com>
2014-09-19 16:55:57 -0700

    IGS Search words filling

--- db_igs_search.php
+++ db_igs_search.php
@@ -44,7 +44,7 @@ if (!$con->set_charset("utf8")) {
 }
 
 
-$sql="SELECT ID,gurmukhi FROM igs where page>2 and page<=100 order by ID";
+$sql="SELECT ID,gurmukhi FROM igs where page<=20 order by ID";
 
 $result = mysqli_query($con, $sql);
 
@@ -58,7 +58,7 @@ while($row = mysqli_fetch_array($result)) {
 	//echo $page . ' - ' . $line . '<br/>';
 	$search = ''; 
 	foreach($words as $word)
-		$search .= ($word[0]=='i')?$word[1]:$word[0];
+		$search .= convert(($word[0]=='i')?$word[1]:$word[0]);
 	
 	echo $text . ' = ' . $search . '<br/>';
 			
@@ -71,6 +71,265 @@ mysqli_query($con, $sql);
 
 mysqli_close($con);
 
+function convert($letter)
+{
+	 switch ($letter)
+	{
+		case 'a':
+			  return 'ੳ';
+		      break;
+		case 'A':
+		      return 'ਅ';
+		      break;
+		case 's':
+		      return 'ਸ';
+		      break;
+		case 'S':
+		      return 'ਸ਼';
+		      break;
+		case 'd':
+		      return 'ਦ';
+		      break;
+		case 'D':
+		      return 'ਧ';
+		      break;
+		case 'f':
+		      return 'ਡ';
+		      break;
+		case 'F':
+		      return 'ਢ';
+		      break;
+		case 'g':
+		      return 'ਗ';
+		      break;
+		case 'G':
+		      return 'ਘ';
+		      break;
+		case 'h':
+		      return 'ਹ';
+		      break;
+//		case 'H':
+//		      return '੍ਹ';
+//		      break;
+		case 'j':
+		      return 'ਜ';
+		      break;
+		case 'J':
+		      return 'ਝ';
+		      break;
+		case 'k':
+		      return 'ਕ';
+		      break;
+		case 'K':
+		      return 'ਖ';
+		      break;
+		case 'l':
+		      return 'ਲ';
+		      break;
+		case 'L':
+		      return 'ਲ਼';
+		      break;
+		case 'q':
+		      return 'ਤ';
+		      break;
+		case 'Q':
+		      return 'ਥ';
+		      break;
+//		case 'w':
+//		      return 'ਾ';
+//		      break;
+//		case 'W':
+//		      return 'ਾਂ';
+//		      break;
+		case 'e':
+		      return 'ੲ';
+		      break;
+		case 'E':
+//		      return 'ਓ';
+		      return 'ੳ';
+		      break;
+		case 'r':
+		      return 'ਰ';
+		      break;
+//		case 'R':
+//        case '®':
+//		      return '੍ਰ';
+//		      break;
+		case 't':
+		      return 'ਟ';
+		      break;
+		case 'T':
+		      return 'ਠ';
+		      break;
+//		case 'y':
+//		      return 'ੇ';
+//		      break;
+//		case 'Y':
+//		      return 'ੈ';
+//		      break;
+//		case 'u':
+//		case 'ü':
+//		      return 'ੁ';
+//		      break;
+//		case 'U':
+//		      return 'ੂ';
+//		      break;
+//		case 'i':
+//		      return 'ਿ';
+//		      break;
+//		case 'I':
+//		      return 'ੀ';
+//		      break;
+//		case 'o':
+//		      return 'ੋ';
+//		      break;
+//		case 'O':
+//		      return 'ੌ';
+//		      break;
+		case 'p':
+		      return 'ਪ';
+		      break;
+		case 'P':
+		      return 'ਫ';
+		      break;
+		case 'z':
+		      return 'ਜ਼';
+		      break;
+		case 'z':
+		      return 'ਗ਼';
+		      break;
+		case 'Z':
+		      return 'ਗ਼';
+		      break;
+		case 'x':
+		      return 'ਣ';
+		      break;
+		case 'X':
+		      return 'ਯ';
+		      break;
+		case 'c':
+		      return 'ਚ';
+		      break;
+		case 'C':
+		      return 'ਛ';
+		      break;
+		case 'v':
+		      return 'ਵ';
+		      break;
+		case 'V':
+		      return 'ੜ';
+		      break;
+		case 'b':
+		      return 'ਬ';
+		      break;
+		case 'B':
+		      return 'ਭ';
+		      break;
+		case 'n':
+		      return 'ਨ';
+		      break;
+//		case 'N':
+//		case 'ˆ':
+//		      return 'ਂ';
+//		      break;
+		case 'm':
+		      return 'ਮ';
+		      break;
+//		case 'M':
+//        case 'µ':
+//		      return 'ੰ';
+//		      break;
+//		case '`':
+//		      return 'ੱ';
+//		      break;
+//        case 'Í':
+//              return '੍ਵ'
+//              break;
+//        case 'ç':
+//              return '੍ਚ'
+//              break;
+//        case '†':
+//              return '੍ਟ'
+//              break;
+//        case 'œ':
+//              return '੍ਤ'
+//              break;
+//        case '~':
+//              return '੍ਨ'
+//              break;
+//        case '´':
+//              return '੍ਯ'
+//              break;
+		case '1':
+//		      return '੧';
+		      return '';
+		      break;
+		case '2':
+//		      return '੨';
+		      return '';
+		      break;
+		case '3':
+//		      return '੩';
+		      return '';
+		      break;
+		case '4':
+//		      return '੪';
+		      return '';
+		      break;
+		case '5':
+//		      return '੫';
+		      return '';
+		      break;
+		case '6':
+//		      return '੬';
+		      return '';
+		      break;
+		case '^':
+		      return 'ਖ਼';
+		      break;
+		case '7':
+//		      return '੭';
+		      return '';
+		      break;
+		case '&':
+		      return 'ਫ਼';
+		      break;
+		case '8':
+//		      return '੮';
+		      return '';
+		      break;
+		case '9':
+//		      return '੯';
+		      return '';
+		      break;
+		case '0':
+//		      return '੦';
+		      return '';
+		      break;
+		case '\\':
+		      return 'ਞ';
+		      break;
+		case '|':
+		      return 'ਙ';
+		      break;
+		case ']':
+//		      return '||';
+			  return '';
+		      break;
+		case '<':
+		      return 'ੴ';
+		      break;
+//		case 'Ú':
+//		      return 'ਃ';
+//		      break;
+//        case '@':
+//		      return '੍ਹ';
+//		      break;
+		default:
+			return -1;
+		  //return charToConvert;
+	}
+}
 
 ?>
 </span>
